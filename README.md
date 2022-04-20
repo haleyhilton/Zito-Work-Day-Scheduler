efb4be9129eb3441eec3fd104bc0f134fefed3c8
Tater <haleymariehilton@gmail.com>
2022-04-19 21:01:05 -0700

    added localSotrage and persistance so that tasks remain after being saved. Changed .attr class to simplified .addClass. Unnested the grid to expand text area to the full width. Eliminated .children in savebtn click function so that it applies to all classes

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Zito-Work-Day-Scheduler
+
+Deployed site: https://haleyhilton.github.io/Zito-Work-Day-Scheduler/
+
+This work day scheduler should function to help an employee (or anyone) manage their busy schedule. Key functionality of this scheduler is as follows:
+
+-moment() updates date daily, backend option to add time
+-input text and events under the time block you plan to execute
+-save will both save the text and persist in localStorage after refreshing the page (should it need to be referenced)
+-timeblocks will change color depending on event due time: past, present and future to ensure urgency
+
+At the end of your day, you can expect your scheduler to look like the below
+
+![End of Day View](assets/images/zito-workday-scheduler.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,113 @@
-var dayDisplayEl = $('#currentDay');
-
-
 //Date and time displayed at the top
 var dayDisplayEl = $('#currentDay')
 setInterval(function () {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    var rightNow = moment().format('MMM DD, YYYY');
     dayDisplayEl.text(rightNow);
 }, 1000);
 
+// grid array
+var todayPlanner = [
+    {
+        id: "0",
+        hour: "09 ",
+        time: "09",
+        meridiem: "am",
+    },
+    {
+        id: "1",
+        hour: "10 ",
+        time: "10",
+        meridiem: "am",
+    },
+    {
+        id: "2",
+        hour: "11 ",
+        time: "11",
+        meridiem: "am",
+    },
+    {
+        id: "3",
+        hour: "12 ",
+        time: "12",
+        meridiem: "pm",
+    },
+    {
+        id: "4",
+        hour: "01 ",
+        time: "13",
+        meridiem: "pm",
+    },
+    {
+        id: "5",
+        hour: "02 ",
+        time: "14",
+        meridiem: "pm",
+    },
+    {
+        id: "6",
+        hour: "03 ",
+        time: "15",
+        meridiem: "pm",
+    },
+    {
+        id: "7",
+        hour: "04 ",
+        time: "16",
+        meridiem: "pm",
+    },
+    {
+        id: "8",
+        hour: "05 ",
+        time: "17",
+        meridiem: "pm",
+    },
+    
+]
 
-// class = container for timeblocks
-//timeblocks for business hours 
+//create grid using row and col
+//set localStorage
+//set if else statement for color change depending on relationship with moment
+todayPlanner.forEach(function(thisHour) {
+    var hourRow = $("<form>").addClass("row")
+   
+    $(".container").append(hourRow);
 
-//color coding to indicate whether it is past, present or future
-// var colorCodingEl function() {
-// if current time block matches rightNow, color is grey
-// if else current time block matches rightNow, color is grey
-//}
+    var hourField = $("<div>").addClass("col-2 hour text-uppercase")
+        .text(`${thisHour.hour}${thisHour.meridiem}`)
+       
+        
+        var planData = $("<textarea>").addClass("col-9 description");
+    
+    var storage = localStorage.getItem(thisHour.id)
+    planData.attr("id", thisHour.id).val(storage)
+    if (thisHour.time < moment().format("HH")) {
+        planData.addClass("past")
+        
+    } else if (thisHour.time === moment().format("HH")) {
+        planData.addClass("present")
+       
+    } else if (thisHour.time > moment().format("HH")) {
+        planData.addClass("future")
+       
+    }
 
+    // save button
+    var saveButton = $("<i class='far fa-save fa-lg'></i>")
+    var savePlan = $("<button>")
+        .attr({
+            "class": "col-1 saveBtn"
+    });
+    savePlan.append(saveButton);
+    hourRow.append(hourField, planData, savePlan);
+})
 
+//complete localStorage
+$(".saveBtn").on("click", function(event) {
+    event.preventDefault();
+    var saveIndex = $(this).siblings(".description").attr("id");
+    var saveText = $(this).siblings(".description").val();
+    console.log(saveIndex);
+    console.log(saveText);
+    localStorage.setItem(saveIndex, saveText)
 
-//click event. Click into a timeblock and you can enter an event
-//save button in that timeblock
-//text for the event is saved in local storage
-//event persists
+})
